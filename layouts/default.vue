08059b29fce1874d577e0d66547d570c78a64c0b
Joseph Mohan <v-joemohan@microsoft.com>
2023-12-21 09:07:30 +0000

    feat: single emitter

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,24 @@
 <script setup>
 // Composables
+import Lenis from '@studio-freight/lenis'
+const { $gsap, $ScrollTrigger } = useNuxtApp()
+
 useStats()
 
 // Methods
 const handleLoadingDone = () => {
   console.log('loading done')
 }
+
+onMounted(() => {
+  const lenis = new Lenis()
+  lenis.on('scroll', $ScrollTrigger.update)
+  $gsap.ticker.add((time) => {
+    lenis.raf(time * 1000)
+  })
+  $gsap.ticker.lagSmoothing(0)
+  lenis.scrollTo(0)
+})
 </script>
 
 <template>
