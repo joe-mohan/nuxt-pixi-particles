4dc00901944cdd8a2aa7a1f37c227685895fc2fb
Joseph Mohan <v-joemohan@microsoft.com>
2023-12-22 14:06:42 +0000

    feat: aspect images

--- pages/images.vue
+++ pages/images.vue
@@ -1,14 +1,65 @@
 <script setup>
-import { Application, Assets, Sprite } from 'pixi.js'
+import { Application, Assets, Sprite, Container, Point, Graphics } from 'pixi.js'
 import { PixelateFilter } from '@pixi/filter-pixelate';
 const { $gsap, $ScrollTrigger } = useNuxtApp()
 
 const mainRef = ref(null)
 const canvasRef = ref(null)
 
+const imgPos = (image, el) => {
+    const x = el.getBoundingClientRect().left 
+    const y = el.getBoundingClientRect().top
+
+    image.x = x
+    image.y = y
 
 
+    // not sure about this
+    const graphics = new Graphics();
+    graphics.beginFill(0xFF3300);
+    graphics.drawRect(x, y, el.offsetWidth, el.offsetHeight);
+    graphics.endFill();
 
+    image.mask = graphics;
+
+    return image
+}
+
+const background = (bgSize, inputSprite, type, forceSize) => {
+    const sprite = inputSprite
+    const bgContainer = new Container({width: bgSize.x, height: bgSize.y})
+    bgContainer.addChild(sprite)
+
+    const self = this
+    function resize() {
+        let sp = { x: sprite.width, y: sprite.height }
+        if (forceSize) sp = forceSize
+        const winratio = bgSize.x / bgSize.y
+        const spratio = sp.x / sp.y
+        let scale = 1
+        const pos = new Point(0, 0)
+        if (type === 'cover' ? winratio > spratio : winratio < spratio) {
+            // photo is wider than background
+            scale = bgSize.x / sp.x
+            pos.y = -(sp.y * scale - bgSize.y) / 2
+        } else {
+            // photo is taller than background
+            scale = bgSize.y / sp.y
+            pos.x = -(sp.x * scale - bgSize.x) / 2
+        }
+
+        sprite.scale = new Point(scale, scale)
+        sprite.position = pos
+    }
+
+    resize()
+
+    return {
+        sprite,
+        container: bgContainer,
+        doResize: resize,
+    }
+}
 
 onMounted(async () => {
     const app = new Application({
@@ -29,36 +80,29 @@ onMounted(async () => {
     const imgEls = mainRef.value.querySelectorAll('.pixi-img')
 
     for (const el of imgEls) {
-        console.log(el)
+
         const texture = await Assets.load(el.src);
-        //console.log('texture', texture)
-        const image = Sprite.from(texture);
-        console.log('image', image)
 
-        const filter = new PixelateFilter({size: 0.1})
-
-        image.filters = [filter];
-
+        var containerSize = { x: el.offsetWidth, y: el.offsetHeight };
+        const image = background(containerSize, Sprite.from(texture), 'cover');
        
+        imgPos(image.container, el)
+
+        const filter = new PixelateFilter()
+        image.sprite.filters = [filter];
 
         $ScrollTrigger.create({
             trigger: el,
-            // animation: entrance,
-            toggleActions: 'play reverse play reverse',
-            onUpdate({ progress, isActive }) {
+            onEnter() {
+                app.stage.addChild(image.container);
+                $gsap.set(filter, { size:  0.1 } )
+            },
+            onUpdate({ isActive }) {
                 if (isActive) {
-                   
-                    const x = el.getBoundingClientRect().left + el.offsetWidth / 2
-                    const y = el.getBoundingClientRect().top + el.offsetHeight / 2
-            
-                    image.anchor.set(0.5, 0.5);
-                    image.width = el.offsetWidth
-                    image.height = el.offsetHeight
-                    image.x = x
-                    image.y = y
-                    app.stage.addChild(image);
+                    imgPos(image.container, el)
+                    app.stage.addChild(image.container);
                 } else {
-                    app.stage.removeChild(image);
+                    app.stage.removeChild(image.container);
                 }
             }
         })
@@ -73,15 +117,9 @@ onMounted(async () => {
                     size: isHovering ? 32 : 0.1,
                     enabled: true,
                     duration: 0.5,
-                    onComplete: () => {
-                        // self.$gsap.ticker.remove(self.renderPixi)
-                        // self.renderer.destroy(true)
-                        // self.canvas = false
-                    },
                 },
                 '>'
             )
-            
         }
 
         el.addEventListener('mouseover', interact);
@@ -93,9 +131,9 @@ onMounted(async () => {
 
 <template>
     <div ref="mainRef" class="container">
-        <canvas ref="canvasRef" class="pointer-events-none fixed inset-0 z-50 h-screen w-screen" />
+        <canvas ref="canvasRef" class="pointer-events-none fixed inset-0 z-0 h-screen w-screen" />
         <section class="relative z-10 py-128 flex min-h-screen items-center gap-128 flex-wrap justify-center text-black">
-            <img class="pixi-img" v-for="y in 10" :key="y" :src="`/${y}.jpeg`"/>
+            <img class="pixi-img opacity-0 aspect-square object-cover" v-for="y in 10" :key="y" :src="`/${y}.jpeg`"/>
         </section>
     </div>
 </template>
